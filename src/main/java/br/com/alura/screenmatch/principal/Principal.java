e08c62493657276421b46603f6c881ccb0994075
Fabricio felix <felixfabricio115@gmail.com>
2025-05-02 11:29:36 -0300

    Criando o menu de interação com o usuário

---
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,23 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.service.ConsumoApi;
+
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner leitura = new Scanner(System.in);
+private ConsumoApi consumo = new ConsumoApi();
+
+
+
+    private  final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private  final String API_KEY = "&apikey=6585022c";
+
+    public void exibeMenu(Principal principal) {
+        System.out.println("Digite o nome da serie");
+        var nomeSerie = leitura.nextLine();
+
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        //https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c
+    }
+}
