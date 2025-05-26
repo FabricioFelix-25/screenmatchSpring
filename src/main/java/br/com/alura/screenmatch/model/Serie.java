90e10ba70399e7204aab0391c67ccd1c5e9536be
Fabricio felix <felixfabricio115@gmail.com>
2025-05-26 20:20:36 -0300

    feito a criacao de tabela serie

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -1,19 +1,25 @@
 package br.com.alura.screenmatch.model;
 
 import br.com.alura.screenmatch.service.ConsultaChatGPT;
-import com.fasterxml.jackson.annotation.JsonAlias;
-import jdk.jfr.Category;
+import jakarta.persistence.*;
 
-import java.util.Optional;
-import java.util.OptionalDouble;
-
+@Entity
+@Table(name = "series")
 public class Serie {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
     private  String titulo;
 
     private Integer totalTemporadas;
 
     private Double avaliacao;
 
+    @Enumerated(EnumType.STRING)
     private Categoria genero;
 
     private String atores;
@@ -23,7 +29,14 @@ public class Serie {
     private String sinopse;
 
 
+    public Serie() {
+
+
+
+    }
+
     public Serie(DadosSerie dadosSerie) {
+
         this.titulo = dadosSerie.titulo();
         this.totalTemporadas = dadosSerie.totalTemporadas();
         try {
@@ -45,7 +58,14 @@ public class Serie {
         this.poster = dadosSerie.poster();
         this.sinopse = ConsultaChatGPT.obterTraducao(dadosSerie.sinopse()).trim();
 
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getTitulo() {
