9f9179fa078f3e2d1c0fc59679924af527d7d030
Fabricio felix <felixfabricio115@gmail.com>
2025-07-23 09:13:32 -0300

    ajuste de chave estrangeira e nome de ep

--- src/main/java/br/com/alura/screenmatch/model/Serie.java
+++ src/main/java/br/com/alura/screenmatch/model/Serie.java
@@ -31,7 +31,7 @@ public class Serie {
 
     private String sinopse;
 
-    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "serie", cascade = CascadeType.ALL, fetch = FetchType.EAGER)
      private List<Episodio> episodios = new ArrayList<>();
 
 
@@ -77,6 +77,8 @@ public class Serie {
     }
 
     public void setEpisodios(List<Episodio> episodios) {
+
+        episodios.forEach(e -> e.setSerie(this));
         this.episodios = episodios;
     }
 
@@ -144,6 +146,7 @@ public class Serie {
                 ", avaliacao=" + avaliacao +
                 ", atores='" + atores + '\'' +
                 ", poster='" + poster + '\'' +
-                ", sinopse='" + sinopse + '\'';
+                ", sinopse='" + sinopse + '\'' +
+                ", episodios='" + episodios + '\'';
     }
 }
