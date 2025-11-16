265f3665e7d636b6d941fdc3e8110f7b41c9124d
FabricioFelix-25 <felixFabricio115@gmail.com>
2025-11-16 11:26:36 -0300

    foi criado o dto do projeto e configurando a ligacao do front ao bakend

--- src/main/java/br/com/alura/screenmatch/controller/SerieController.java
+++ src/main/java/br/com/alura/screenmatch/controller/SerieController.java
@@ -1,13 +1,14 @@
 package br.com.alura.screenmatch.controller;
 
 
-import br.com.alura.screenmatch.model.Serie;
+import br.com.alura.screenmatch.dto.SerieDTO;
 import br.com.alura.screenmatch.repository.SerieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
@@ -16,7 +17,12 @@ public class SerieController {
     private SerieRepository serieRepository;
 
     @GetMapping("/series")
-    public List<Serie> obterSeries() {
-        return serieRepository.findAll();
+    public List<SerieDTO> obterSeries() {
+        return serieRepository.findAll().stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
     }
+
+
+
 }
